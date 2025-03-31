f22b5ae30a721e7a340c9dda83149434dd0696ce
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2025-04-01 00:46:13 +0200

    Add files via upload

---
+++ framework-16.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+
+let
+  nixos-hardware = builtins.fetchTarball https://github.com/NixOS/nixos-hardware/archive/master.tar.gz;
+in
+
+{
+  imports = [
+    (import "${nixos-hardware}/framework/16-inch/7040-amd")
+  ];
+  
+  fileSystems."/mnt/Games" =
+  { device = "/dev/disk/by-uuid/1b35568b-4447-4c80-9880-4b359d4ecb6c";
+    fsType = "ext4";
+  };
+ 
+}
