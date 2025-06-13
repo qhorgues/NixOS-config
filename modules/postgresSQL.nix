aa1b4df8d6ba0ff11038aadb83c848857c7ff02c
Quentin Horgues <quentin.horgues@ikmail.com>
2025-06-13 23:21:06 +0200

    restore default settings for postgres

--- modules/postgresSQL.nix
+++ modules/postgresSQL.nix
@@ -6,18 +6,26 @@
     settings = {
         listen_addresses = lib.mkForce "127.0.0.1";
     };
-    authentication = ''
-        # TYPE  DATABASE        USER            ADDRESS                 METHOD
-        local   all             user1sa                                 md5
-        local   all             user2bdr                                md5
-        local   all             user3		                                md5
-        local   all             user4		                                md5
-        local   all             user5		                                md5
-        local   all             user6		                                md5
-        host    all             all             127.0.0.1/32            trust
-        host    all             all             ::1/128                 trust
-        local   all             all                                     trust
-    '';
+    # authentication = ''
+    #     # TYPE  DATABASE        USER            ADDRESS                 METHOD
+    #     local   all             user1sa                                 md5
+    #     local   all             user2bdr                                md5
+    #     local   all             user3		                                md5
+    #     local   all             user4		                                md5
+    #     local   all             user5		                                md5
+    #     local   all             user6		                                md5
+    #     local   replication     postgres                                trust
+    #     host    all             all             127.0.0.1/32            trust
+    #     host    all             all             ::1/128                 trust
+    #     local   all             all                                     trust
+    # '';
+    # settings = {
+    #   archive_mode = "on";
+    #   restore_command = "cp /tmp/wal_archive/%f \"%p\"";
+    #   archive_command = "test ! -f /var/lib/postgresql/wal_archive/%f && cp %p /var/lib/postgresql/wal_archive/%f";
+    #   wal_level = "replica";
+    #   archive_timeout = 60;
+    # };
   };
 
   systemd.services.postgresql = {
