3b99947d285cf5803c8d407bc8811af022926fe8
Quentin Horgues <quentin.horgues@ikmail.com>
2025-06-20 18:17:01 +0200

    update channel and fix auto update

--- modules/update.nix
+++ modules/update.nix
@@ -1,17 +1,28 @@
-{ ... }:
+{ self, ... }:
 
 {
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.dates = "weekly";
-  system.autoUpgrade.allowReboot = true;
-  system.autoUpgrade.rebootWindow =
-  {
-      lower = "02:00";
-      upper = "06:00";
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+    allowReboot = true;
+    flake = self.outPath;
+    flags = [
+        "--update-input"
+        "nixpkgs"
+        "home-manager"
+        "nixos-hardware"
+      ];
+    rebootWindow =
+    {
+        lower = "02:00";
+        upper = "06:00";
+    };
   };
 
-  nix.gc.automatic = true;
-  nix.gc.dates = "daily";
-  nix.gc.options = "--delete-older-than 5d";
   nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 5d";
+  };
 }
