ee9761614b60b279c877474e8f7efc5bb8dc6014
qhorgues <quentin.horgues@outlook.fr>
2025-07-12 11:33:27 +0200

    update and remove auto update

--- modules/nixos/update.nix
+++ modules/nixos/update.nix
@@ -1,31 +1,31 @@
-{ ... }:
+{ lib, config, ... }:
 
 {
-  system.autoUpgrade = {
-    enable = true;
-    dates = "weekly";
-    allowReboot = true;
-    flake = "$WINTER_CONFIG_PATH";
-    flags = [
-        "--update-input"
-        "nixpkgs"
-        "--update-input"
-        "home-manager"
-        "--update-input"
-        "nixos-hardware"
-      ];
-    rebootWindow =
-    {
-        lower = "02:00";
-        upper = "06:00";
-    };
-  };
+    #options.winter.auto-upgrade.enable = lib.mkOption {
+    #  description = "Enable auto upgrade";
+    #  type = lib.types.bool;
+    #  default = false;
+    #};
 
-  nix.settings.auto-optimise-store = true;
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than 5d";
-  };
-  services.fwupd.enable = true;
+    #config = lib.mkIf config.winter.auto-upgrade.enable {
+    # system.autoUpgrade = {
+    #     enable = true;
+    #     dates = "weekly";
+    #     allowReboot = true;
+    #     flake = "";
+    #     flags = [];
+    #     rebootWindow =
+    #     {
+    #         lower = "02:00";
+    #         upper = "06:00";
+    #     };
+    # };
+
+    nix.settings.auto-optimise-store = true;
+    nix.gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 5d";
+    };
+    services.fwupd.enable = true;
 }
