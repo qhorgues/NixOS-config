5dbd815ae1aaf2f6e3ef2d55466ba66d8cbf3a26
qhorgues <quentin.horgues@outlook.fr>
2025-07-24 23:49:06 +0200

    update and fix shortcut for terminal

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -105,6 +105,7 @@
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
         ];
       };
       "org/gnome/Console" = {
