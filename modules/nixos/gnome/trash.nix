75f5b2d17404e6ea903626ed4a938db80d487cd5
qhorgues <quentin.horgues@outlook.fr>
2025-08-12 14:37:04 +0200

    fix invalid name

--- modules/nixos/gnome/trash.nix
+++ modules/nixos/gnome/trash.nix
@@ -2,14 +2,14 @@
 
 {
   options = {
-    enableTarsh = lib.mkOption {
+    enableTrash = lib.mkOption {
         type = lib.types.bool;
         default = true;
         description = "enable trash support";
     };
     };
 
-  config = lib.mkIf config.enableTarsh {
+  config = lib.mkIf config.enableTrash {
     environment.sessionVariables.GIO_EXTRA_MODULES = lib.mkForce "${config.services.gvfs.package}/lib/gio/modules";
       environment.variables.GIO_EXTRA_MODULES = lib.mkForce config.environment.sessionVariables.GIO_EXTRA_MODULES;
   };
