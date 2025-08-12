ddce0cb3ea043953f45bc406e89c243e64480702
qhorgues <quentin.horgues@outlook.fr>
2025-08-12 14:36:23 +0200

    fix mangohud limit framerate and clock

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -8,8 +8,10 @@
     };
     gamemode.enable = true;
     steam = {
-      gamescopeSession.enable = true;
       enable = true;
+      gamescopeSession = {
+          enable = true;
+      };
       extest.enable = true;
       remotePlay.openFirewall = false;
       dedicatedServer.openFirewall = false;
@@ -19,6 +21,7 @@
       ];
       package = pkgs.steam.override {
         extraEnv = {
+          TZ = ":/etc/localtime";
           MANGOHUD = true;
         };
       };
@@ -27,7 +30,7 @@
   environment = {
     sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-      MANGOHUD_CONFIG = "control=mangohud,hud_no_margin,legacy_layout=false,horizontal,background_alpha=0.6,round_corners=0,background_alpha=0.2,background_color=000000,font_size=24,text_color=FFFFFF,position=top-center,toggle_hud=Shift_R+F12,no_display,table_columns=1,gpu_text=GPU,gpu_stats,gpu_temp,gpu_power,gpu_color=2E9762,cpu_text=CPU,cpu_stats,cpu_temp,cpu_power,cpu_color=2E97CB,vram,vram_color=AD64C1,vram_color=AD64C1,ram,ram_color=C26693,battery,battery_color=00FF00,fps,gpu_name,wine,wine_color=EB5B5B,fps_limit_method=late,toggle_fps_limit=Shift_R+F1,fps_limit=60,time";
+      MANGOHUD_CONFIG = "control=mangohud,hud_no_margin,legacy_layout=false,horizontal,round_corners=0,background_alpha=0.2,background_color=000000,font_size=24,text_color=FFFFFF,position=top-center,toggle_hud=Shift_R+F12,no_display,table_columns=1,gpu_text=GPU,gpu_stats,gpu_temp,gpu_power,gpu_color=2E9762,cpu_text=CPU,cpu_stats,cpu_temp,cpu_power,cpu_color=2E97CB,vram,vram_color=AD64C1,ram,ram_color=C26693,battery,battery_color=00FF00,fps,gpu_name,wine,wine_color=EB5B5B,fps_limit_method=late,toggle_fps_limit=Shift_R+F1,fps_limit=165\\,60,show_fps_limit,time";
     };
   };
   environment.systemPackages = with pkgs; [
