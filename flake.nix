837bc529a4e89bbfe517de89f0de1f744e482ce9
qhorgues <quentin.horgues@outlook.fr>
2025-08-26 22:26:53 +0200

    add in commentary winteros-detect-hardware

--- flake.nix
+++ flake.nix
@@ -11,41 +11,56 @@
         url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
         inputs.nixpkgs.follows = "nixpkgs";
     };
+    # winteros-detect-hardware = {
+    #     url = "github:Winter-OS/winteros-detect-hardware/master";
+    #     inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
   let
-    system = "x86_64-linux";
     nixpkgsConfig = {
         allowUnfree = true;
-    };
-    pkgs-unstable = import nixpkgs-unstable {
-        inherit system;
-        config = nixpkgsConfig;
     };
   in
   {
     nixosConfigurations =
     {
-      "fw-laptop-16" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit self inputs pkgs-unstable; };
+      "fw-laptop-16" = let system = "x86_64-linux"; in nixpkgs.lib.nixosSystem
+      {
+        system = system;
+        specialArgs = { inherit self inputs;
+            pkgs-unstable = import nixpkgs-unstable {
+                system = system;
+                config = nixpkgsConfig;
+            };
+        };
         modules = [
             ./hosts/fw-laptop-16/configuration.nix
             inputs.home-manager.nixosModules.default
         ];
       };
-      "unowhy-13" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit self inputs pkgs-unstable; };
+      "unowhy-13" = let system = "x86_64-linux";in nixpkgs.lib.nixosSystem {
+        system = system;
+        specialArgs = { inherit self inputs;
+            pkgs-unstable = import nixpkgs-unstable {
+                system = system;
+                config = nixpkgsConfig;
+            };
+        };
         modules = [
             ./hosts/unowhy-13/configuration.nix
             inputs.home-manager.nixosModules.default
         ];
       };
-      "desktop-acer-n50" = nixpkgs.lib.nixosSystem {
-       	system = "x86_64-linux";
-       	specialArgs = { inherit self inputs pkgs-unstable; };
+      "desktop-acer-n50" = let system = "x86_64-linux";in nixpkgs.lib.nixosSystem {
+       	system = system;
+       	specialArgs = { inherit self inputs;
+            pkgs-unstable = import nixpkgs-unstable {
+                system = system;
+                config = nixpkgsConfig;
+            };
+        };
        	modules = [
             ./hosts/desktop-acer-n50/configuration.nix
             inputs.home-manager.nixosModules.default
