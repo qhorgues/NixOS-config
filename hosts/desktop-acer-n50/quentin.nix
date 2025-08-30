d1feb124a81d36b0c7cdd1eb7016f123c1e56470
qhorgues <quentin.horgues@outlook.fr>
2025-08-31 00:29:36 +0200

    add modeling module

--- hosts/desktop-acer-n50/quentin.nix
+++ hosts/desktop-acer-n50/quentin.nix
@@ -12,6 +12,7 @@
     ../../modules/home-manager/office.nix
     ../../modules/home-manager/vm-manager.nix
     ../../modules/home-manager/flake-script.nix
+    ../../modules/home-manager/modeling.nix
     ./home-manager/zed-remote-folder.nix
   ];
 
