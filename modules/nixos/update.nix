06f95582413e9e4385bbb6b4656981cb46b927b1
qhorgues <quentin.horgues@outlook.fr>
2025-11-08 18:54:58 +0100

    increase time between auto clean

--- modules/nixos/update.nix
+++ modules/nixos/update.nix
@@ -4,8 +4,8 @@
     nix.settings.auto-optimise-store = true;
     nix.gc = {
         automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 5d";
+        dates = "weekly";
+        options = "--delete-older-than 14d";
     };
     services.fwupd.enable = true;
 }
