8cff98ca9a522749cf553dcd05b4927cd3a60552
qhorgues <quentin.horgues@outlook.fr>
2025-11-11 14:27:35 +0100

    add kernel optimization

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,4 +1,4 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, pkgs-unstable, lib, ... }:
 
 {
   programs = {
@@ -43,6 +43,44 @@
         package = pkgs.mesa;
         package32 = pkgs.pkgsi686Linux.mesa;
       };
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      linuxPackages = super.linuxPackages // {
+        kernel = super.linuxPackages.kernel.override {
+          structuredExtraConfig = with lib.kernel; {
+            HZ_1000 = yes;
+            HZ = 1000;
+            PREEMPT_FULL = yes;
+            IOSCHED_BFQ = yes;
+            DEFAULT_BFQ = yes;
+            DEFAULT_IOSCHED = "bfq";
+            V4L2_LOOPBACK = module;
+            HID = yes;
+          };
+        };
+      };
+    })
+  ];
+
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="block", ATTR{queue/scheduler}="bfq"
+  '';
+
+  boot.kernel.sysctl = {
+    "kernel.split_lock_mitigate" = 0;
+    "vm.swappiness" = 10;
+    "vm.vfs_cache_pressure" = 50;
+    "vm.dirty_bytes" = 268435456;
+    "vm.max_map_count" = 16777216;
+    "vm.dirty_background_bytes" = 67108864;
+    "vm.dirty_writeback_centisecs" = 1500;
+    "kernel.nmi_watchdog" = 0;
+    "kernel.unprivileged_userns_clone" = 1;
+    "kernel.printk" = "3 3 3 3";
+    "kernel.kptr_restrict" = 2;
+    "kernel.kexec_load_disabled" = 1;
   };
 
 #  system.activationScripts.steamConfigInject = {
