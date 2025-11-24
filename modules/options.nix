e00c262b651c4f4c2d74a3397cbffa8e96a25e85
qhorgues <quentin.horgues@outlook.fr>
2025-11-24 01:58:09 +0100

    add option for lossless scaling and remove unused option

--- modules/options.nix
+++ modules/options.nix
@@ -18,4 +18,17 @@ with lib;
     default = null;
     description = "Acceleration (cuda, rocm)";
   };
+
+  options.winter.games.lsfg = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable Losseless Scaling (required Lossless scaling app on Steam)";
+    };
+    steam_library_for_lossless_scaling = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Path to lossless scaling DLL";
+    };
+  };
 }
