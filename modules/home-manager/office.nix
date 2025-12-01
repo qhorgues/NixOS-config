6d0236ef3bb756edf823463396e7a94be6b31659
qhorgues <quentin.horgues@outlook.fr>
2025-12-01 22:49:03 +0100

    fix desktop integration in texstudio

--- modules/home-manager/office.nix
+++ modules/home-manager/office.nix
@@ -3,7 +3,15 @@
 {
   home.packages = with pkgs; [
     texliveFull
-    texstudio
+    (pkgs.texstudio.overrideAttrs (oldAttrs: {
+        nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
+        postFixup = ''
+          wrapProgram $out/bin/texstudio \
+            --prefix XDG_DATA_DIRS : "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}" \
+            --prefix XDG_DATA_DIRS : "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+        '';
+      }))
+    gsettings-desktop-schemas
     onlyoffice-desktopeditors
     libreoffice
     thunderbird-latest-bin
