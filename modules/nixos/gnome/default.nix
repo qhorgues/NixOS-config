3dce667b9884a378d80cd69ede563b871ab1c579
qhorgues <quentin.horgues@outlook.fr>
2025-12-01 22:50:34 +0100

    add GSConect

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -115,6 +115,12 @@
             ];
         }
         {
+          networking.firewall = rec {
+            allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+            allowedUDPPortRanges = allowedTCPPortRanges;
+          };
+        }
+        {
             # Is used to fix blank screen on GDM
 
             systemd.services."getty@tty1".enable = false;
