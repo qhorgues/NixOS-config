b8ace9a1cb8bc038164e24fab6773e6a8495958a
qhorgues <quentin.horgues@outlook.fr>
2025-12-08 13:00:25 +0100

    add output in nix-update script

--- pkgs/nix-update.nix
+++ pkgs/nix-update.nix
@@ -7,13 +7,13 @@ pkgs.writeShellScriptBin "nix-update" ''
       exit 1
     fi
 
-    ${pkgs.nix}/bin/nix flake update --flake ${flake_path} > /dev/null 2>&1
+    ${pkgs.nix}/bin/nix flake update --flake ${flake_path}
 
     ${pkgs.git}/bin/git add flake.lock > /dev/null 2>&1
     ${pkgs.git}/bin/git commit -m "update" > /dev/null 2>&1
 
 
-    if sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${flake_path}\#${flake_config} > /dev/null 2>&1; then
+    if sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${flake_path}\#${flake_config}; then
       ${pkgs.git}/bin/git push > /dev/null 2>&1
       echo "✅ Mise à jour avec succès"
       ${nix-latest-update}/bin/nix-latest-update
