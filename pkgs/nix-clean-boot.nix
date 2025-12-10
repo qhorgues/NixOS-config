50bf5cf6cb3e7dfbede450610fe7fa9b8a8e3f85
qhorgues <quentin.horgues@outlook.fr>
2025-12-10 16:38:50 +0100

    use full path in flake script

--- pkgs/nix-clean-boot.nix
+++ pkgs/nix-clean-boot.nix
@@ -3,7 +3,7 @@
 pkgs.writeShellScriptBin "nix-clean-boot" ''
     ${pkgs.nix}/bin/nix flake update --flake ${flake_path}
     ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${flake_path}\#${flake_config}
-    rm /nix/var/nix/gcroots/auto/*
-    nix-store --gc
-    nix-collect-garbage -d
+    ${pkgs.coreutils}/bin/rm /nix/var/nix/gcroots/auto/*
+    ${pkgs.nix}/bin/nix-store --gc
+    ${pkgs.nix}/bin/nix-collect-garbage -d
 ''
