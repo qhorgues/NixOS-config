615258420264b199700f37262b5a35482bf9a506
qhorgues <quentin.horgues@outlook.fr>
2025-12-16 17:05:04 +0100

    refactor

--- modules/options.nix
+++ modules/options.nix
@@ -1,22 +1,30 @@
 { lib, ... }:
 with lib;
 {
-  options.winter.hardware.gpu.vendor = mkOption {
-    type = types.nullOr types.str;
-    default = null;
-    description = "GPU constructor (amdgpu, nvidia, intel)";
-  };
+  options.winter.hardware.gpu = {
+    vendor = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "GPU constructor (amdgpu, nvidia, intel)";
+    };
 
-  options.winter.hardware.gpu.enable-acceleration = mkOption {
-    type = types.bool;
-    default = false;
-    description = "enable gpu acceleration (cuda, rocm)";
-  };
+    enable-acceleration = mkOption {
+      type = types.bool;
+      default = false;
+      description = "enable gpu acceleration (cuda, rocm)";
+    };
 
-  options.winter.hardware.gpu.acceleration = mkOption {
-    type = types.nullOr types.str;
-    default = null;
-    description = "Acceleration (cuda, rocm)";
+    acceleration = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Acceleration (cuda, rocm)";
+    };
+
+    generation = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "GPU génération ()";
+    };
   };
 
   options.winter.games.lsfg = {
