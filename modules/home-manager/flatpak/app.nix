61c6aafb1be25dbbaec3e51dce5e9faae6730f4f
qhorgues <quentin.horgues@outlook.fr>
2025-12-17 22:16:09 +0100

    replace by add for install flatpak

--- modules/home-manager/flatpak/app.nix
+++ modules/home-manager/flatpak/app.nix
@@ -4,6 +4,6 @@ appId:
   lib.hm.dag.entryAfter ["flatpak"]
     ''
       if ! ${pkgs.flatpak}/bin/flatpak list --user | ${pkgs.gnugrep}/bin/grep -q "${appId}"; then
-        ${pkgs.flatpak}/bin/flatpak install --user -y flathub ${appId}
+        ${pkgs.flatpak}/bin/flatpak add --user -y flathub ${appId}
       fi
     ''
