211f3dc176a234a34830cc789917d130ba542724
qhorgues <quentin.horgues@outlook.fr>
2025-12-28 01:19:27 +0100

    move vm option to global option file

--- modules/nixos/core/options.nix
+++ modules/nixos/core/options.nix
@@ -51,4 +51,24 @@ with lib;
     default = false;
     description = "Enable fan control for framework";
   };
+
+  options.winter.vm = {
+    users = mkOption {
+      type = with types; listOf str;
+      default = [];
+      description = "Utilisateurs pour lesquels activer les permissions liées à la virtualisation.";
+    };
+
+    # platform = mkOption {
+    #   type = types.str;
+    #   default = "";
+    #   description = "Plateforme CPU (ex: amd ou intel) pour configurer les options IOMMU.";
+    # };
+
+    # vfioIds = mkOption {
+    #   type = with types; listOf str;
+    #   default = [];
+    #   description = "Liste des identifiants PCI des périphériques à passer via VFIO.";
+    # };
+  };
 }
