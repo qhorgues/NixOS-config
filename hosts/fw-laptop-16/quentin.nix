aaccbf0212e3ce643b05493e3b70b3db956155fb
qhorgues <quentin.horgues@outlook.fr>
2026-01-10 00:47:47 +0100

    remove unused module and app

--- hosts/fw-laptop-16/quentin.nix
+++ hosts/fw-laptop-16/quentin.nix
@@ -7,16 +7,16 @@
     ../../modules/home-manager/firefox
     ../../modules/home-manager/gnome
     ../../modules/home-manager/kdrive.nix
-    ../../modules/home-manager/graphism.nix
+    # ../../modules/home-manager/graphism.nix
     ../../modules/home-manager/zed.nix
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/dev.nix
     ../../modules/home-manager/shell.nix
-    ../../modules/home-manager/office.nix
+    # ../../modules/home-manager/office.nix
     # ../../modules/home-manager/vm-manager.nix
     ../../modules/home-manager/flake-script.nix
-    ../../modules/home-manager/vscode.nix
-    ../../modules/home-manager/vim.nix
+    # ../../modules/home-manager/vscode.nix
+    # ../../modules/home-manager/vim.nix
     ../../modules/home-manager/discord.nix
     ./home-manager/zed-remote-folder.nix
   ];
