6351e5d32ab9df5c65cb23c8f57b99c59200ee71
qhorgues <quentin.horgues@outlook.fr>
2026-01-14 16:18:03 +0100

    change zram to fully use ram space and change swapiness also to 100 see: https://github.com/CachyOS/CachyOS-Settings/pull/19

--- modules/nixos/core/zram.nix
+++ modules/nixos/core/zram.nix
@@ -1,10 +1,13 @@
 { lib, ... }:
 
 {
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 100;
+  };
   zramSwap = {
     enable = lib.mkDefault true;
     algorithm = "zstd";
-    memoryPercent = lib.mkDefault 30;
+    memoryPercent = lib.mkDefault 100;
     priority = 5;
   };
 }
