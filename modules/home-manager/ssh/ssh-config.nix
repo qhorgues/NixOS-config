82b89e35755a0dfc958491f19e792ab5786f286e
qhorgues <quentin.horgues@outlook.fr>
2026-01-21 15:25:42 +0100

    change hostname in ssh config

--- modules/home-manager/ssh/ssh-config.nix
+++ modules/home-manager/ssh/ssh-config.nix
@@ -13,7 +13,7 @@ in
           user = "quentin";
           identityFile = "~/.ssh/id_ed25519";
         };
-        "rpi-quentin-polytech" = {
+        "rpi-quentin-proxy" = {
           hostname = "91.165.146.203";
           port = 48320;
           user = "quentin";
