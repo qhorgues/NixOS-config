c594db96a34fb60aa2af2fefdc05dddc9be2aa2b
qhorgues <quentin.horgues@outlook.fr>
2026-03-06 03:40:14 +0100

    add rollback fix for GPU

--- hosts/fw-laptop-16/configuration.nix
+++ hosts/fw-laptop-16/configuration.nix
@@ -80,7 +80,22 @@
         ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="32ac", ATTRS{idProduct}=="0018", ATTR{power/wakeup}="disabled"
     '';
 
-
+    # Fix for AMD GPU crash
+    nixpkgs.overlays = [
+      (self: super:
+        let
+          version = "20250808";
+        in
+        {
+        linux-firmware = super.linux-firmware.overrideAttrs (old: {
+          version = version;
+          src = super.fetchurl {
+            url = "https://cdn.kernel.org/pub/linux/kernel/firmware/linux-firmware-${version}.tar.xz";
+            sha256 = "sha256-wClVG0WhWSbJ16XfGgtUAEQGTxkVfFf8Edkf0Kreg38=";
+          };
+        });
+      })
+    ];
 
     programs.adb.enable = true;
     users.users."quentin".extraGroups = [ "adbusers" ];
