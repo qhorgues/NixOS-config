b7749313bbe951ea8889748d6fbb33cb33366d13
qhorgues <quentin.horgues@outlook.fr>
2026-03-22 12:33:01 +0100

    add option for bootloader

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -1,27 +1,43 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
+let
+  cfg = config.mx.bootloader;
+in
 {
-  boot = {
-    loader.limine = {
-      enable = true;
-      maxGenerations = 10;
-      secureBoot.enable = true;
-      extraConfig = "timeout: 1\nquiet: yes\nremember_last_entry: yes";
+  options.mx.bootloader = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Use default bootloader";
     };
+  };
+  config = {
+    boot = lib.mkMerge [
+      (
+        lib.mkIf cfg.enable {
+          loader.limine = {
+            enable = true;
+            maxGenerations = 10;
+            secureBoot.enable = true;
+            extraConfig = "timeout: 1\nquiet: yes\nremember_last_entry: yes";
+          };
 
-    loader.efi.canTouchEfiVariables = lib.mkDefault true;
-    tmp.useTmpfs = lib.mkDefault true;
-    consoleLogLevel = 3;
-    initrd.verbose = false;
+          loader.efi.canTouchEfiVariables = lib.mkDefault true;
+        }
+      )
+      {
+        kernelPackages = lib.mkDefault pkgs.linuxPackages;
+        initrd.verbose = false;
+        tmp.useTmpfs = lib.mkDefault true;
+        kernelParams = lib.mkDefault [
+          "quiet"
+          "udev.log_level=3"
+          "iommu=pt" # Fix pour certain cpu AMD
+        ];
 
-    kernelPackages = lib.mkDefault pkgs.linuxPackages;
-    kernelParams = lib.mkDefault [
-      "quiet"
-      "udev.log_level=3"
-      "iommu=pt" # Fix pour certain cpu AMD
+        initrd.systemd.enable = lib.mkDefault true;
+        plymouth.enable = lib.mkDefault true;
+      }
     ];
-
-    initrd.systemd.enable = lib.mkDefault true;
-    plymouth.enable = lib.mkDefault true;
   };
 }
