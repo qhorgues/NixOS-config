4be27a9b3ab189ae037edb88a10cd739f8d34369
qhorgues <quentin.horgues@outlook.fr>
2026-04-05 21:54:50 +0200

    add nvidia 595 driver

--- modules/nixos/core/nvidia.nix
+++ modules/nixos/core/nvidia.nix
@@ -17,6 +17,16 @@ let
     settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
     persistencedSha256 = "sha256-il403KPFAnDbB+dITnBGljhpsUPjZwmLjGt8iPKuBqw=";
   };
+
+  nvidia595Driver = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "595.58.03";
+    sha256_64bit = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
+    sha256_aarch64 = "sha256-hzzIKY1Te8QkCBWR+H5k1FB/HK1UgGhai6cl3wEaPT8=";
+    openSha256 = "sha256-6LvJyT0cMXGS290Dh8hd9rc+nYZqBzDIlItOFk8S4n8=";
+    settingsSha256 = "sha256-2vLF5Evl2D6tRQJo0uUyY3tpWqjvJQ0/Rpxan3NOD3c=";
+    persistencedSha256 = "sha256-AtjM/ml/ngZil8DMYNH+P111ohuk9mWw5t4z7CHjPWw=";
+  };
+
 in
 {
   config = lib.mkMerge [
@@ -31,7 +41,7 @@ in
           else if legacy-fermi then
             config.boot.kernelPackages.nvidiaPackages.lecay_390
           else
-            config.boot.kernelPackages.nvidiaPackages.beta
+            nvidia595Driver
           );
       }
     )
