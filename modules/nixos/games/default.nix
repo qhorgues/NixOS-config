5027597f847754ba5950078d61ef93e0d47808ba
qhorgues <quentin.horgues@outlook.fr>
2026-06-01 13:53:42 +0700

    remove unused fix

--- modules/nixos/games/default.nix
+++ modules/nixos/games/default.nix
@@ -1,4 +1,4 @@
-{ config, self, pkgs, pkgs-unstable, lib, ... }:
+{ config, pkgs, pkgs-unstable, lib, ... }:
 
 let
   cfg = config.mx.programs.games;
@@ -83,17 +83,6 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    nixpkgs.overlays = [
-      (_final: prev: let
-        stable = import self.inputs.nixpkgs {
-          inherit (prev.stdenv.hostPlatform) system;
-          config.allowUnfree = true;
-        };
-        in {
-        inherit (stable) bubblewrap;
-        })
-    ];
-
     programs = {
       gamescope = {
         enable = true;
