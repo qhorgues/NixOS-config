27f52174e9a765f9fbf60218d58b82bd810b98d2
qhorgues <quentin.horgues@outlook.fr>
2026-06-08 21:03:46 +0700

    Add audio low latency and noise suppression

--- modules/nixos/core/sound.nix
+++ modules/nixos/core/sound.nix
@@ -16,10 +16,65 @@ in
     services.pulseaudio.enable = false;
     services.pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       pulse.enable = true;
       jack.enable = true;
+
+      # Based on GLF OS configuration
+      extraConfig.pipewire = {
+        "91-low-latency" = {
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.quantum" = 256;
+            "default.clock.min-quantum" = 256;
+            "default.clock.max-quantum" = 256;
+          };
+        };
+        "92-noise-suppression" = {
+          "context.modules" = [{
+            name = "libpipewire-module-filter-chain";
+            flags = [ "nofail" ];
+            args = {
+              "node.description" = "Noise Canceling Source";
+              "media.name" = "Noise Canceling Source";
+              "filter.graph" = {
+                nodes = [{
+                  type = "ladspa";
+                  name = "rnnoise";
+                  plugin = "librnnoise_ladspa";
+                  label = "noise_suppressor_stereo";
+                  control = { "VAD Threshold (%)" = 50.0; };
+                }];
+              };
+              "capture.props" = {
+                "node.name" = "effect_input.rnnoise";
+                "node.passive" = true;
+                "audio.rate" = 48000;
+                "audio.position" = [ "FL" "FR" ];
+              };
+              "playback.props" = {
+                "node.name" = "rnnoise_source";
+                "node.description" = "Noise Canceling Source";
+                "media.class" = "Audio/Source";
+                "audio.rate" = 48000;
+                "audio.position" = [ "FL" "FR" ];
+              };
+            };
+          }];
+        };
+      };
+      wireplumber.extraConfig = {
+        "10-disable-camera" = {
+          "wireplumber.profiles" = {
+            main = {
+              "monitor.libcamera" = "disabled";
+            };
+          };
+        };
+      };
     };
   };
 }
