cf063340b930cdae49c8832104a897b17ace9dcc
qhorgues <quentin.horgues@outlook.fr>
2026-06-11 11:45:26 +0700

    [update] Proton CachyOS SLR

--- pkgs/proton-cachyos.nix
+++ pkgs/proton-cachyos.nix
@@ -6,7 +6,7 @@
   # Can be overridden to alter the display name in steam
   # This could be useful if multiple versions should be installed together
   steamDisplayName ? "Proton CachyOS",
-  version ? "11.0-20260521-slr"
+  version ? "11.0-20260601-slr"
 }:
 let
   tag = "cachyos-${version}";
@@ -17,7 +17,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   src = fetchzip {
     url = "https://github.com/CachyOS/proton-cachyos/releases/download/${tag}/proton-cachyos-${version}-x86_64.tar.xz";
-    hash = "sha256-OrZfLv0FV+fhDuhbmcbMhtylDTMwTfRAvENh/v7iIr8=";
+    hash = "sha256-WkQRPX7sw/OnphLek5XGSrJTY8hrECCJY9zUaw6/jdA=";
   };
 
 
