b9e809fc691d2530e67af160a7d13e51fc1e8ea7
qhorgues <quentin.horgues@outlook.fr>
2026-06-16 13:17:40 +0700

    move secret file

--- secrets.nix
+++ secrets.nix
@@ -8,11 +8,13 @@ let
 
   host-rpi-horgues = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYn/xadR0hDoNy8CG7DYNZSju31zyPRxutjpDeejh6W root@nixos-installer";
 
+  fw-laptop-16 = [ quentin-fw-laptop-16 host-fw-laptop-16 ];
+
   allHosts = [ host-fw-laptop-16 host-rpi-horgues ];
   allKeys  = [ quentin-fw-laptop-16 quentin-rpi-horgues ] ++ allHosts;
 in
 {
   # Secret spécifique à chaque machine
-  "fw-laptop-16/wireguard-key.age".publicKeys  = [ quentin-fw-laptop-16 host-fw-laptop-16 ];
-
+  "secrets/fw-laptop-16/wireguard-key.age".publicKeys  = fw-laptop-16;
+  "secrets/shared/hungingface-token.age".publicKeys = fw-laptop-16;
 }
