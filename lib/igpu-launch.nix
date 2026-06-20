729103bf86bb01962b4e7ae4ba3a656c57b1ed25
qhorgues <quentin.horgues@outlook.fr>
2026-06-20 12:57:43 +0700

    fix invalid param name

--- lib/igpu-launch.nix
+++ lib/igpu-launch.nix
@@ -1,12 +1,12 @@
 { writeShellScriptBin
 , igpuId
-, gpuNumber
+, igpuNumber
 }:
 
 writeShellScriptBin "igpu-launch" ''
   export MESA_VK_DEVICE_SELECT="${igpuId}!"
   export DRI_PRIME=0
-  export MANGOHUD_CONFIG="$(echo "$MANGOHUD_CONFIG" | sed 's/gpu_list=[^,]*/gpu_list=${toString gpuNumber}/' || echo "$MANGOHUD_CONFIG")"
+  export MANGOHUD_CONFIG="$(echo "$MANGOHUD_CONFIG" | sed 's/gpu_list=[^,]*/gpu_list=${toString igpuNumber}/' || echo "$MANGOHUD_CONFIG")"
 
   exec "$@"
 ''
