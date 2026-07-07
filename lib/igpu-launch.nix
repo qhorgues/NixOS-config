12a4ff380f2ca7363ab9fdfa8a70b62e6b02d8c6
qhorgues <quentin.horgues@outlook.fr>
2026-07-07 11:28:59 +0700

    fix mangohud

--- lib/igpu-launch.nix
+++ lib/igpu-launch.nix
@@ -6,7 +6,13 @@
 writeShellScriptBin "igpu-launch" ''
   export MESA_VK_DEVICE_SELECT="${igpuId}!"
   export DRI_PRIME=0
-  export MANGOHUD_CONFIG="$(echo "$MANGOHUD_CONFIG" | sed 's/gpu_list=[^,]*/gpu_list=${toString igpuNumber}/' || echo "$MANGOHUD_CONFIG")"
+
+  gpu_count="$(find /sys/class/drm -maxdepth 1 -name 'card[0-9]*' | grep -cE '/card[0-9]+$')"
+  if [ "$gpu_count" -gt 1 ]; then
+    export MANGOHUD_CONFIG="$(echo "$MANGOHUD_CONFIG" | sed 's/gpu_list=[^,]*/gpu_list=${toString igpuNumber}/' || echo "$MANGOHUD_CONFIG")"
+  else
+    export MANGOHUD_CONFIG="$(echo "$MANGOHUD_CONFIG" | sed -E 's/(^|,)gpu_list=[^,]*//' | sed -E 's/^,//')"
+  fi
 
   exec "$@"
 ''
