9d397ca0ac1128d0a081ba488c2cf30d7ce9d5bc
qhorgues <quentin.horgues@outlook.fr>
2026-08-22 09:43:22 +0200

    add global option for enable all office tools

--- modules/home-manager/quentin/office/default.nix
+++ modules/home-manager/quentin/office/default.nix
@@ -1,9 +1,24 @@
-{ ... }:
+{ config, lib, ... }:
 
+let
+  cfg = config.mx.programs.office;
+in
 {
+  options.mx.programs.office = {
+    enable = lib.mkEnableOption "Enable all office tools";
+  };
+
   imports = [
     ./latex.nix
     ./onlyoffice.nix
     ./libre-office.nix
   ];
+
+  config = lib.mkIf cfg.enable {
+    mx.programs.office = {
+      latex.enable = lib.mkDefaut true;
+      onlyoffice.enable = lib.mkDefaut true;
+      libre-office.enable = lib.mkDefaut true;
+    };
+  };
 }
